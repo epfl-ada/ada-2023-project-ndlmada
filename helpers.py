658b9aec838e04c912dcead4aed7b973aa4c1d49
Julie Korber <juliekorber@MacBook-Pro-de-Julie.local>
2023-11-16 18:47:31 +0100

    add preprocessing function

--- helpers.py
+++ helpers.py
@@ -8,21 +8,71 @@ from bs4 import BeautifulSoup # To Extract all the URLs from the HTML page
 def change_characters(dict_df, dataset_name, column_name):
     """ It replaces the %xx escapes with their single-character equivalent for all the value of a specified column.
     param:
-        dict_df: dictionnary of dataframes
-        dataset_name: str, name of the dataset contain in dict_df
-        column_name: str, name of the column from which we want to replace the values
+        dict_df: dict
+            dictionnary of dataframes
+        dataset_name: str
+            name of the dataset contain in dict_df
+        column_name: str
+            name of the column from which we want to replace the values
     return:
         dict_df: the provided dictionnary of dataframe with the replaced characters
      """
     dict_df[dataset_name][column_name] = [unquote(art) for art in dict_df[dataset_name][column_name]]
     return dict_df
 
+
+def preprocessing():
+    """ It downloads the dataset from wikispeedia paths-and-graph and applied the preprocessing
+        return:
+            dfs: a dictionary with the preprocessed dataset.
+     """
+    ## Download the data: 
+    
+    # Paths
+    folder_path = "dataset/wikispeedia_paths-and-graph/"
+    file_paths = ["paths_finished.tsv", "paths_unfinished.tsv", "categories.tsv", "articles.tsv", "links.tsv"]
+
+    # Datasets: Names and their columns name
+    data_frames_names = ["paths_finished", "paths_unfinished", "categories", "article", "links"]
+    dfs_headers = [
+        ["hashedIpAddress", "timestamp", "durationInSec", "path", "rating"],
+        ["hashedIpAddress", "timestamp", "durationInSec", "path", "target", "type"],
+        ["article", "category"],
+        ["article"],
+        ["linkSource", "linkTarget"]
+    ]
+
+    dfs_skiprows = [16, 17, 13, 12, 12]
+    dfs = {}
+    for i in range(len(file_paths)):
+        dfs[data_frames_names[i]] = pd.read_csv(folder_path + file_paths[i], sep='\t', header=None, names=dfs_headers[i], skiprows=range(dfs_skiprows[i]))
+    
+    ## Preprocessing 
+    dfs = change_characters(dfs, 'paths_finished', 'path')
+    dfs['paths_finished'] = dfs['paths_finished'].drop(['hashedIpAddress', 'rating'], axis = 1)
+
+    dfs = change_characters(dfs, 'paths_unfinished', 'path')
+    dfs = change_characters(dfs, 'paths_unfinished', 'target')
+
+    dfs = change_characters(dfs, 'categories', 'article')
+    dfs = change_characters(dfs, 'categories', 'category')
+
+    dfs = change_characters(dfs, 'article', 'article')
+
+    dfs = change_characters(dfs, 'links', 'linkSource')
+    dfs = change_characters(dfs, 'links', 'linkTarget')
+
+    return dfs
+
+
 def extract_links(file_path):
     """It extracts all the links there is in the provided article. 
     param:
-        file_path: str, path to the article we want to access.
+        file_path: str
+            path to the article we want to access.
     return:
-       filtered_links: list of str, the list of all the hyperlink (only those link to another article) found in the provided article.
+       filtered_links: list of str
+            the list of all the hyperlink (only those link to another article) found in the provided article.
     """
     with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
         soup = BeautifulSoup(file, 'html.parser')
@@ -43,7 +93,8 @@ def path_to_name(path):
     param: 
         path: str
     return: 
-        name: str, name of the article extract from the path
+        name: str
+            name of the article extract from the path
      """
     
     # Extract last path name of the source article
